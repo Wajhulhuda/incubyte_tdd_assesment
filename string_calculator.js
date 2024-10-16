ee4c1d491cffa521f173e37774d236a1c8b42fef
Wajhulhuda <wajhulhuda50@gmail.com>
2024-10-16 20:14:21 +0530

    cheked for empty string

---
+++ string_calculator.js
@@ -0,0 +1,11 @@
+// Test
+describe('String Calculator', () => {
+    it('should return 0 for an empty string', () => {
+        expect(add("")).toBe(0);
+    });
+});
+
+// Code
+function add(numbers) {
+    return 0;
+}
